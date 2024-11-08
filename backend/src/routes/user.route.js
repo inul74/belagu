dbe0850bea32061b716f0441ca4311a791eef452
inul74 <zmusthofa74@gmail.com>
2024-11-08 16:55:37 +0700

    user, stat routes & controllers

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers, getMessages } from "../controller/user.controller.js";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  req.auth.userId;
-  res.send("User route with get method");
-});
+router.get("/", protectRoute, getAllUsers);
+router.get("/messages/:userId", protectRoute, getMessages);
 
 export default router;
